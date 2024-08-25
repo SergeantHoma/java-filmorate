96b8812c2df795abfc5be0a55f3198e364f007e9
Valdimir Esin <vova.esin.00@mail.ru>
2024-08-25 22:59:17 +0300

    1

--- src/main/java/ru/yandex/practicum/filmorate/exeption/UserException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/UserException.java
@@ -1,5 +1,10 @@
 package ru.yandex.practicum.filmorate.exeption;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UserException extends RuntimeException {
     public UserException(String massage) {
         super(massage);
